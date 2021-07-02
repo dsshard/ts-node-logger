8379ead0020b35e140acbed16a0f3cbf8f6cc0b7
andrey <andrey@coinsocialstory.com>
2021-07-02 15:43:24 +0300

    remove ^ in package.json and add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,16 @@
+interface LoggerConstructorParams {
+    name: string;
+}
+export declare function InjectLogger(ctr: Function): void;
+export declare class Logger {
+    private readonly prefix;
+    private uuid;
+    constructor(params?: LoggerConstructorParams);
+    private message;
+    resetId(): void;
+    log(...args: any[]): void;
+    warn(...args: any[]): void;
+    error(...args: any[]): void;
+    info(...args: any[]): void;
+}
+export {};
